8c2c64dbce75cbf6a1567953ead28915bf9ac0ba
Jovan275 <jovanconti86@gmail.com>
2021-09-16 14:52:03 +0200

    Uradjen navbar i nav meni

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,16 +1,24 @@
+import {useState} from "react";
 import Topbar from "./components/topbar/Topbar";
 import Intro from "./components/intro/Intro";
 import Portfolio from "./components/portfolio/Portfolio";
 import Testimonials from "./components/testimonials/Testimonials";
 import Works from "./components/works/Works";
 import Contact from "./components/contact/Contact";
+import Menu from "./components/menu/Menu";
 import "./App.scss";
 
+
 function App() {
+
+  const [menuOpen, setMenuOpen] =  useState(false); /* ovo nam sluzi za hamburger meni da kad kliknemo da menja boju
+  inicijalno je na false tj bice nam bele boje pa kad kliknemo onda ce biti plave*/ 
+
   return (
     
     <div className="app">
-      <Topbar/>
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <div className="sections">
         <Intro/>
         <Portfolio/>
--- portfolio/src/components/topbar/Topbar.jsx
+++ portfolio/src/components/topbar/Topbar.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
 import "./Topbar.scss";
+import {Person, Mail} from "@material-ui/icons";
 
-function Topbar() {
+function Topbar({menuOpen, setMenuOpen}) {
+    /*logika za navbar je u prvom divu*/ 
     return (
-        <div className="topbar">
+        <div className={"topbar " + (menuOpen && "active")}>
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">genius.</a>
+                    <div className="item-container">
+                        <Person className="icon"/>
+                        <span>+381 60/727-05-98</span>
+                    </div>
+                    <div className="item-container">
+                        <Mail className="icon"/>
+                        <span>jovanconti86@gmail.com</span>
+                    </div>
+                </div>
+            
+            <div className="right">
+                <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}> 
+                    <span className="line1"></span>
+                    <span className="line2"></span>
+                    <span className="line3"></span>
+                </div>
+            </div>
+            </div>
             
         </div>
     )
