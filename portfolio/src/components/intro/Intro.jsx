18cc68dc01f421ad47475ff07918120c40937764
Jovan275 <jovanconti86@gmail.com>
2021-09-17 12:20:18 +0200

    Uradjena sekcija intro

--- portfolio/src/components/intro/Intro.jsx
+++ portfolio/src/components/intro/Intro.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import "./Intro.scss";
+import { init } from 'ityped';
 
 function Intro() {
+
+    const textRef = useRef(); /*ovo useRef se ponasa slicno kao oni selektori tj isto radi kao i oni*/ 
+
+
+    useEffect(() => {
+
+        init(textRef.current, { showCursor: true, backDelay:1500, backSpeed:60, strings: ['Developer', 'Dizajner', 'Kreator sadrzaja'] });
+        
+    }, [])
+
     return (
         <div className="intro" id="intro">
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/man.png" alt=""></img>
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Zdravo, moje ime je</h2>
+                    <h1>Jovan Conti</h1>
+                    <h3>Freelance <span ref={textRef}></span></h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/down.png" alt=""></img>
+                </a>
+            </div>
             
         </div>
     )
--- portfolio/src/components/menu/Menu.jsx
+++ portfolio/src/components/menu/Menu.jsx
@@ -8,16 +8,16 @@ function Menu({menuOpen, setMenuOpen}) {
                 <li onClick={() => setMenuOpen(false)}>
                     <a href="#intro">Home</a>
                 </li>
-                <li v>
+                <li onClick={() => setMenuOpen(false)}>
                     <a href="#portfolio">Portfolio</a>
                 </li>
-                <li v>
+                <li onClick={() => setMenuOpen(false)}>
                     <a href="#works">Works</a>
                 </li>
                 <li onClick={() => setMenuOpen(false)}>
                     <a href="#testimonials">Testimonials</a>
                 </li>
-                <li v>
+                <li onClick={() => setMenuOpen(false)}>
                     <a href="#contact">Contact</a>
                 </li>
             </ul>
